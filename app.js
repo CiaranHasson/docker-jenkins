af354ebcdd799746d09995e019e9fe30d3fd56f1
Ciaran Hasson <ciaranha@ciarans-mbp.home>
2021-03-18 16:56:02 +0000

    added some orwell

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Ciaran / Azure / version 2');
+  res.send('It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him.');
 });
 
 app.listen(config.port, () => {
